0dfb713d46af257c8a63849d7ac2f6e36193dadd
Carlos Urquiza <pnw.carlos@gmail.com>
2022-10-12 16:44:32 -0700

    add new script to practice using page.click with playwright

---
+++ click.js
@@ -0,0 +1,35 @@
+const { chromium } = require('playwright');
+
+  ( async() => {
+    //function code
+    const browser = await chromium.launch({headless: false, slowMo: 500});
+    const page = await browser.newPage();
+    await page.goto('https://www.apronus.com/music/lessons/unit01.htm');
+    // click on the keynotes to hear a master piece
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(3) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(5) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(1) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(1) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(3) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(5) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(1) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(5) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(6) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(8) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(5) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(6) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(8) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(8) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(10) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(8) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(6) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(5) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(1) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(8) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(10) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(8) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(6) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(5) > button');
+    await page.click('#t1 > table > tr:nth-child(1) > td:nth-child(1) > button');
+    await browser.close();
+})();
